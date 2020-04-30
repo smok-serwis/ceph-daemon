f77ae506de62c95a680a203434ec56d6c04cc11b
Piotr Maślanka <piotr.maslanka@henrietta.com.pl>
2020-04-30 19:40:12 +0200

    last commit, now it works

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,15 +2,15 @@
 set -e
 
 if [ -z "$MON_IP" ]; then
-  LINES=$(hostname -i | tr " " "\n")
-  for item in "$LINES"
-  do
+  hostname -i | tr " " "\n" > addresses.txt
+  while read item; do
     if [[ "$item" =~ [0-9]+\.[0-9]+\.[0-9]+\.[0-9]+ ]];
     then
+      echo "Choosing $item as IPv4 address"
       export MON_IP=$item
       exec /opt/ceph-container/bin/entrypoint.sh "$@"
     fi
-  done
+  done < addresses.txt
   echo "Failed to discover an IPv4! Options were: " $(hostname -i)
   exit 1
 else
